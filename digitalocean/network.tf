cba942b357bdbebc8dd292f155152ce1428c0441
iuriikogan <koganiurii@gmail.com>
2024-01-10 12:30:18 +0200

    rm lb def

--- digitalocean/network.tf
+++ digitalocean/network.tf
@@ -5,21 +5,3 @@ resource "digitalocean_vpc" "portfolio-vpc" {
 
   ip_range = "192.168.0.0/16"
 }
-
-resource "digitalocean_loadbalancer" "portfolio_loadbalancer" {
-  name   = "portfolio-lb"
-  region = "ams3"
-
-  forwarding_rule {
-    entry_port     = 443
-    entry_protocol = "https"
-
-    target_port     = 443
-    target_protocol = "https"
-    tls_passthrough = true
-  }
-} 
-
-output "portfolio_lb_id" {
-  value = digitalocean_loadbalancer.portfolio_loadbalancer.id
-}
